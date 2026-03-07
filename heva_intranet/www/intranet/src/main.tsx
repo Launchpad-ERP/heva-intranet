bdd0c0ef67c9916d7948e2219426d1451b4626b8
Sanny Nguyen Hung <sannynguyen90@googlemail.com>
2026-03-07 10:51:21 +0100

    add manifest for pwa

--- heva_intranet/www/intranet/src/main.tsx
+++ heva_intranet/www/intranet/src/main.tsx
@@ -8,3 +8,16 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <App />
     </React.StrictMode>,
 )
+
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js', { scope: '/' })
+            .then(registration => {
+                console.log('SW registered: ', registration);
+            })
+            .catch(registrationError => {
+                console.log('SW registration failed: ', registrationError);
+            });
+    });
+}
